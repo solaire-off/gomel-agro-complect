1bf5c61fa55b037f69880bff33dbbf3a6a927608
solaire-off <victor374288@mail.ru>
2018-01-05 14:54:05 +0300

    Update admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Item, Category
 
-admin.site.register(Item)
-admin.site.register(Category)
 
+class ItemAdmin(admin.ModelAdmin):
+    models = Item
+    list_display = ('title','description','category','published','created_date')
+    list_filter = ('published','category', 'created_date')
+    search_fields = ['title','description']
+    list_per_page = 10
 
+class CategoryAdmin(admin.ModelAdmin):
+    models = Category
+    list_display = ('title','description','created_date')
+    list_filter = ('created_date',)
+    search_fields = ['title','description']
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 admin.site.site_header = 'ОАО "Гомельагрокомплект"'
 admin.site.index_title = 'Панель администрации'
