e4a7c96f6a5954d22c9b06f6db4c692423553db5
solaire-off <victor374288@mail.ru>
2018-01-25 23:29:41 +0300

    Add action 'export orders as xls'

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Order
+from .views import export_orders_as_xls
 
 class OrderAdmin(admin.ModelAdmin):
     models = Order
@@ -7,6 +8,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('processed', 'created_date')
     list_editable = ['processed']
     search_fields = ['name','phone','note']
-    list_per_page = 10
+    actions = [export_orders_as_xls]
+    list_per_page = 20
+
 
 admin.site.register(Order, OrderAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+import xlwt
 
-# Create your views here.
+def export_orders_as_xls(modeladmin, request, queryset):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="orders.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Заказы')
+
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+
+    columns = ['Имя', 'Телефон','Примечание','Товар','Принята','Дата']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    font_style = xlwt.XFStyle()
+
+    alignment = xlwt.Alignment()
+
+    alignment.horz = xlwt.Alignment.HORZ_LEFT
+    alignment.vert = xlwt.Alignment.VERT_TOP
+    alignment.wrap = 1
+
+
+    font_style.alignment = alignment
+
+    max_col_width = [15,15,30,30,10,15]
+
+    for obj in queryset:
+        row_num += 1
+
+        processed = 'Нет'
+        if obj.processed : processed = 'Да'
+
+        local_datetime = timezone.localtime(obj.created_date).strftime("%Y-%m-%d %H:%M:%S")
+
+        note = obj.note
+        if obj.note is None:
+            note = 'Отсутствует'
+
+        ws.write(row_num, 0, obj.name, font_style)
+        ws.write(row_num, 1, obj.phone, font_style)
+        ws.write(row_num, 2, note, font_style)
+        ws.write(row_num, 3, obj.item.title, font_style)
+        ws.write(row_num, 4, processed, font_style)
+        ws.write(row_num, 5, local_datetime, font_style)
+
+        if len(note) > 29 :
+            note_hight = round(len(note) / 30)
+            ws.row(row_num).height_mismatch = True
+            ws.row(row_num).height = 255 * (note_hight)
+
+    for col_num in range(len(max_col_width)):
+        ws.col(col_num).width = (max_col_width[col_num]+1) * 280
+
+    wb.save(response)
+    return response
+
+export_orders_as_xls.short_description = "Экспортировать в XLS"
