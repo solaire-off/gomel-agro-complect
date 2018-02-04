248d5f562b186de50acd2fc499bfcbc71a2a5c64
solaire-off <victor374288@mail.ru>
2018-02-04 13:42:32 +0300

    Add search and pagination

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from .models import Item, Category, Detail
 from orders.forms import OrderForm
 
+
 def items_list(request):
-    items = Item.objects.filter(published=True)
-    category = Category.objects.filter(published=True)
+    items = Item.objects.filter(published=True).order_by('-created_date')
+    category = Category.objects.filter(published=True).order_by('title')
+    query = request.GET.get('q')
+    if query:
+        items = Item.objects.filter(
+            Q(published=True),
+            Q(title__icontains=query) |
+            Q(description__icontains=query)
+            ).distinct().order_by('-created_date')
+
+    paginator = Paginator(items,8)
+    page = request.GET.get('page')
+    try:
+        items = paginator.page(page)
+    except PageNotAnInteger:
+        items  = paginator.page(1)
+    except EmptyPage:
+        items = paginator.page(paginator.num_pages)
     context = {
-        'items':items,
-        'category':category
+        'query' : query,
+        'items': items,
+        'category': category
     }
     return render(request, 'catalog/catalog_items.html', context)
 
@@ -22,8 +42,8 @@ def details_list(request):
 
 def items_by_category(request, category_url):
     selected_category = get_object_or_404(Category, url=category_url)
-    items = Item.objects.filter(category=selected_category)
-    category = Category.objects.filter(published=True)
+    items = Item.objects.filter(category=selected_category).order_by('-created_date')
+    category = Category.objects.filter(published=True).order_by('title')
     context = {
         'category':category,
         'selected_category': selected_category,
@@ -35,7 +55,7 @@ def items_by_category(request, category_url):
 def single_item(request, category_url, item_url):
     item = get_object_or_404(Item,url=item_url)
     if not item.published : raise Http404
-    category = Category.objects.filter(published=True)
+    category = Category.objects.filter(published=True).order_by("title")
     details = Detail.objects.filter(items=item)
     order_form = OrderForm
     context = {
