6f23156312bbdac9e816039cd0051122471de41d
solaire-off <victor374288@mail.ru>
2018-02-17 17:06:16 +0300

    Add filter news by tag

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^news/$', views.news_list, name="news_list"),
+    url(r'^news/(?P<tag_url>[-\w]+)/$',views.news_by_tag, name="news_by_tag")
 ]
 
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import News
+from django.shortcuts import get_object_or_404
+from .models import News, Tag
 
 def news_list(request):
     news = News.objects.filter(published=True).order_by('-created_date')
@@ -7,3 +8,12 @@ def news_list(request):
             'news' : news
             }
     return render(request,'news/news.html', context)
+
+def news_by_tag(request, tag_url):
+    selected_tag = get_object_or_404(Tag, url=tag_url)
+    news = News.objects.filter(tags__url__startswith=selected_tag.url, published=True).order_by('-created_date')
+    context = {
+            'news': news,
+            'selected_tag' : selected_tag
+    }
+    return render(request, 'news/news.html',context)
